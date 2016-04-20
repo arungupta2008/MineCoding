c30aa31722d0821019d7609509bbc1fe5cde03c9
Arun Kumar Gupta <arun.gupta@flipkart.com>
2016-04-20 23:50:42 +0530

    BUILD: Added more Algorithms Problem

---
+++ src/Algorithm/Trie/Trie.java
@@ -0,0 +1,75 @@
+package Algorithm.Trie;
+
+/**
+ * Created by arun.gupta on 20/04/16.
+ * * Copied From  :: https://community.oracle.com/thread/2070706
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Trie {
+    private TrieNode root;
+
+    /**
+     * Constructor
+     */
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    /**
+     * Adds a word to the Trie
+     *
+     * @param word
+     */
+    public void addWord(String word) {
+        root.addWord(word.toLowerCase());
+    }
+
+    /**
+     * Get the words in the Trie with the given
+     * prefix
+     *
+     * @param prefix
+     * @return a List containing String objects containing the words in
+     * the Trie with the given prefix.
+     */
+    public List getWords(String prefix) {
+        //Find the node which represents the last letter of the prefix
+        TrieNode lastNode = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            lastNode = lastNode.getNode(prefix.charAt(i));
+
+            //If no node matches, then no words exist, return empty list
+            if (lastNode == null) return new ArrayList();
+        }
+        //Return the words which eminate from the last node
+        return lastNode.getWords();
+    }
+
+    public TrieNode getNextTrieNode(char character){
+        TrieNode lastNode = root;
+        return lastNode.getNode(character);
+    }
+
+    public TrieNode getNextTrieNode(char character, TrieNode trieNode){
+        return trieNode.getNode(character);
+    }
+
+    public static void main(String[] args) {
+        Trie trie = new Trie();
+        trie.trieTest();
+    }
+
+    private void trieTest() {
+        System.out.println("Trie Testing");
+        Trie trie = new Trie();
+        trie.addWord("Arun");
+        trie.addWord("Ajay");
+        trie.addWord("Guay");
+
+        System.out.println("Words : "+ trie.getWords("a"));
+        System.out.println("No Words Should come: "+ trie.getWords("u"));
+    }
+}
---
+++ src/AlgorithmProblems/TrieProblem/Problem1.java
@@ -0,0 +1,61 @@
+package AlgorithmProblems.TrieProblem;
+
+import Algorithm.Trie.Trie;
+import Algorithm.Trie.TrieNode;
+import Helper.Vertex;
+
+import java.io.InputStreamReader;
+import java.util.*;
+
+/**
+ * Created by arun.gupta on 20/04/16.
+ */
+/*
+* Given a stream of characters (e.g. acacabcatghhellomvnsdb) and a list of words (e.g. ["aca","cat","hello","world"] ) find and display count of each and every word once the stream ends.(Like : "aca" : 2 , "cat" : 1 , "hello" : 1 , "world" : 0 ).
+* */
+public class Problem1 {
+
+    public static void main(String[] args) {
+        Trie trie = new Trie();
+        Scanner scanner = new Scanner(new InputStreamReader(System.in));
+        System.out.print("Only enter alphabets : ");
+        String input = "";
+        do {
+            input = scanner.nextLine();
+            trie.addWord(input);
+            System.out.println("Want to enter more? [Y/N]");
+            input = scanner.nextLine();
+        } while (input.equalsIgnoreCase("Y"));
+
+        List<TrieNode> trieNodeList = new ArrayList<>();
+
+        System.out.print("Please enter Input Stream : ");
+        input = scanner.nextLine();
+        for (char latter : input.toCharArray()) {
+
+            List<TrieNode> trieNodesIterator = new ArrayList<>(trieNodeList);
+            int i = 0;
+            for (TrieNode trieNodeIt : trieNodesIterator) {
+                TrieNode nextTrieNode = trie.getNextTrieNode(latter, trieNodeIt);
+                if (nextTrieNode == null) {
+                    trieNodeList.remove(trieNodeIt);
+                } else if (nextTrieNode.isLeaf()) {
+                    System.out.println("Found a Dictionary : " + nextTrieNode.toString());
+                }else {
+                    trieNodeList.set(i,nextTrieNode);
+                }
+                ++i;
+            }
+
+            TrieNode trieNode = trie.getNextTrieNode(latter);
+            if (trieNode == null) {
+
+            } else if (trieNode.isLeaf()) {
+                System.out.println("Found a Dictionary : " + trieNode.toString());
+            } else {
+                trieNodeList.add(trieNode);
+            }
+        }
+
+    }
+}
